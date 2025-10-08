cb326659875532fac0af9c6bb01fe090f0acb1ef
206512542_nbcuni <Scott.Cegielski@dreamworks.com>
2025-10-08 08:08:38 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -17,7 +17,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '15.15'
+    _version = '15.16'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -81,9 +81,9 @@ for i, variant in enumerate(variants):
 requires = [
     'arras4_core-4.10',
     'freetype-2',
-    'mcrt_denoise-6.15',
-    'mcrt_messages-14.5',
-    'scene_rdl2-15.14'
+    'mcrt_denoise-6.16',
+    'mcrt_messages-14.6',
+    'scene_rdl2-15.15'
 ]
 
 private_build_requires = [
